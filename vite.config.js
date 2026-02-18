e50add2e328bfafb8c5885d117c351d5a5dac7bb
snehasiskhan2000-source <snehasiskhan2000@gmail.com>
2026-02-18 18:32:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 10000,
+    allowedHosts: true // This allows all hosts, including Render and your domain
+  }
 })
