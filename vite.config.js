a5a7b370d0b30537ddea4e7258cabaa0062e8ec9
snehasiskhan2000-source <snehasiskhan2000@gmail.com>
2026-02-19 15:46:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    host: true,
-    port: 10000,
-    allowedHosts: true // This allows all hosts, including Render and your domain
-  }
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
