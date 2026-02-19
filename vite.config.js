4ba2f03cfe03fb4427d1d6a8174ce39a46e1ca27
snehasiskhan2000-source <snehasiskhan2000@gmail.com>
2026-02-19 15:56:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000
+  }
 })
